381f510729c5f894fda9a5e8feb91bba17605402
chloro-pn <13125187405@163.com>
2023-08-02 02:31:44 +0800

    refactor switcher

--- doc/switcher.md
+++ doc/switcher.md
@@ -5,8 +5,5 @@ switcher是tunnel中的协调组件，用来为不同进程、不同计算节点
 * switcher不需要是唯一的
 * switcher是无状态的
 * switcher是基于topic的
-* switcher不直接接收或者发送pipeline中传递的数据，只提供协调服务，即为一对目标为同一个topic的push方与pop方建立tcp通道提供服务。
 * switcher是惰性的-防止反压
 * sdk是异步的-便于封装为协程
-* 每一对push与pop独占一条tcp通道（ap场景下对fd的消耗有限）
-* sdk是进程内唯一的
--- include/switcher/switcher.h
+++ include/switcher/switcher.h
@@ -2,6 +2,7 @@
 #define TUNNEL_SWITCHER_H
 
 #include <array>
+#include <limits>
 #include <memory>
 #include <string>
 #include <unordered_map>
@@ -14,24 +15,25 @@ namespace tunnel {
 
 constexpr int request_type_push = 0x00;
 constexpr int request_type_pop = 0x01;
+constexpr int request_type_check_topic_exist = 0x02;
 
 struct request_package {
-  int type;  // push, pop, check, query, ...
   std::string topic;
-  // for push and pop message, meta_info == "sdk_ip:sdk_port"
-  std::string meta_info;
+  int type;  // push, pop, check, query, ...
+  // will add more information, like push/pop_ip:port、push/pop pipeline name etc for debug
 };
 
 struct response_package {
-  int type;
   std::string topic;
-  int role;               // for type == push and pop, role == 0 -> client, role == 1 -> server
-  std::string meta_info;  // for type == push and pop, meta_info == server's ip:port.
+  // only for push and pop request
+  int wait_ms;
+  // only for push and pop request
+  std::string peer_addr;
 };
 
-struct pending_process {
-  std::unique_ptr<asio::ip::tcp::socket> socket;
-  std::string ipport;  // "ip:port"
+struct data_package {
+  std::string data;
+  std::string meta_info;
 };
 
 class Switcher {
@@ -58,45 +60,59 @@ class Switcher {
   }
 
   asio::awaitable<void> handle(socket&& s) {
-    // read_request
-    request_package pkg = co_await read_package<request_package>(s);
+    bool eof = false;
+    request_package pkg = co_await read_package<request_package>(s, eof);
+    if (eof == true) {
+      // closed by client
+      co_return;
+    }
     if (pkg.type == request_type_push) {
-      handle_push_request(std::move(s), pkg);
+      handle_push_request(std::move(s), std::move(pkg));
     } else if (pkg.type == request_type_pop) {
-      handle_pop_request(std::move(s), pkg);
+      handle_pop_request(std::move(s), std::move(pkg));
     } else {
       throw std::runtime_error("invalid request type for switcher now.");
     }
   }
 
-  void handle_push_request(socket&& s, const request_package& pkg) {
-    auto it = pop_requests_.find(pkg.topic);
-    if (it == pop_requests_.end() || it->second.empty()) {
-      pending_process pp;
-      pp.ipport = pkg.meta_info;
-      pp.socket = std::make_unique<socket>(std::move(s));
-      push_requests_[pkg.topic].emplace_back(std::move(pp));
+  void handle_push_request(socket&& s, request_package&& pkg) {
+    auto it = datas_.find(pkg.topic);
+    if (it == datas_.end() || it->second.poping_nodes.empty()) {
+      datas_[pkg.topic].pushing_nodes.emplace_back(std::make_unique<socket>(std::move(s)));
     } else {
-      auto popper = std::move(it->second.back());
-      pop_requests_[pkg.topic].pop_back();
-      co_spawn(ctx_, create_channel(pkg.topic, pkg.meta_info, popper.ipport, std::move(s), std::move(*popper.socket)),
-               asio::detached);
+      auto socket_ptr = std::move(it->second.poping_nodes.back());
+      it->second.poping_nodes.pop_back();
+      socket& pop_socket = *socket_ptr;
+      asio::co_spawn(ctx_, transfer_data(std::move(s), std::move(pop_socket)), asio::detached);
     }
   }
 
-  void handle_pop_request(socket&& s, const request_package& pkg) {
-    auto it = push_requests_.find(pkg.topic);
-    if (it == push_requests_.end() || it->second.empty()) {
-      pending_process pp;
-      pp.ipport = pkg.meta_info;
-      pp.socket = std::make_unique<socket>(std::move(s));
-      pop_requests_[pkg.topic].emplace_back(std::move(pp));
+  void handle_pop_request(socket&& s, request_package&& pkg) {
+    auto it = datas_.find(pkg.topic);
+    if (it == datas_.end() || it->second.pushing_nodes.empty()) {
+      datas_[pkg.topic].poping_nodes.emplace_back(std::make_unique<socket>(std::move(s)));
     } else {
-      auto pusher = std::move(it->second.back());
-      push_requests_[pkg.topic].pop_back();
-      co_spawn(ctx_, create_channel(pkg.topic, pusher.ipport, pkg.meta_info, std::move(*pusher.socket), std::move(s)),
-               asio::detached);
+      auto socket_ptr = std::move(it->second.pushing_nodes.back());
+      it->second.pushing_nodes.pop_back();
+      socket& push_socket = *socket_ptr;
+      asio::co_spawn(ctx_, transfer_data(std::move(push_socket), std::move(s), pkg.topic), asio::detached);
     }
+  }
+
+  // read data from push_socket and write to pop_socket
+  asio::awaitable<void> transfer_data(socket&& push_socket, socket&& pop_socket, std::string topic) {
+    data_package dpkg = co_await read_package<data_package>(push_socket);
+    co_await write_package<data_package>(pop_socket, dpkg);
+    response_package rpkg;
+    rpkg.peer_addr = get_remote_ipport(pop_socket);
+    rpkg.topic = topic;
+    rpkg.wait_ms = 0;  // todo
+    co_await write_package<response_package>(push_socket, rpkg);
+    rpkg.peer_addr = get_remote_ipport(push_socket);
+    co_await write_package<response_package>(pop_socket, rpkg);
+    // try to handle next request
+    asio::co_spawn(ctx_, handle(std::move(push_socket)), asio::detached);
+    asio::co_spawn(ctx_, handle(std::move(pop_socket)), asio::detached);
   }
 
   template <typename T>
@@ -110,29 +126,27 @@ class Switcher {
   }
 
   template <typename T>
+  asio::awaitable<T> read_package(socket& s, bool& eof) {
+    asio::error_code ec;
+    std::array<char, sizeof(uint32_t)> buf;
+    co_await asio::read(s, asio::buffer(buf), asio::use_awaitable, ec);
+    if (ec == asio::error::eof) {
+      eof = true;
+      co_return;
+    }
+    uint32_t length = Deserialize<uint32_t>(std::string_view(&buf[0], buf.size()));
+    std::string buffer(length, '/0');
+    co_await asio::read(s, asio::buffer(buffer), asio::use_awaitable);
+    co_return Deserialize<T>(buffer);
+  }
+
+  template <typename T>
   asio::awaitable<void> write_package(socket& s, const T& pkg) {
     auto buf = Serialize<T>(pkg);
+    assert(buf.size() <= std::numeric_limits<uint32_t>::max());
     auto length_buf = Serialize<uint32_t>(buf.size());
     co_await asio::write(s, asio::buffer(length_buf), asio::use_awaitable);
     co_await asio::write(s, asio::buffer(buf), asio::use_awaitable);
-    co_return;
-  }
-
-  asio::awaitable<void> create_channel(const std::string& topic, const std::string& push_ip_port,
-                                       const std::string& pop_ip_port, socket&& push_socket, socket&& pop_socket) {
-    response_package rpkg;
-    rpkg.type = request_type_pop;
-    rpkg.role = 1;
-    rpkg.topic = topic;
-    rpkg.meta_info = pop_ip_port;
-    co_await write_package<response_package>(pop_socket, rpkg);
-    pop_socket.shutdown(socket::shutdown_send);
-    pop_socket.close();
-    rpkg.role = 0;
-    rpkg.type = request_type_push;
-    co_await write_package<response_package>(push_socket, rpkg);
-    push_socket.shutdown(socket::shutdown_send);
-    push_socket.close();
     co_return;
   }
 
@@ -145,10 +159,15 @@ class Switcher {
   }
 
  private:
+  struct pending_nodes {
+    std::vector<std::unique_ptr<socket>> pushing_nodes;
+    std::vector<std::unique_ptr<socket>> poping_nodes;
+  };
+
   asio::io_context& ctx_;
+  size_t data_capacity_;
   endpoint ep_;
-  std::unordered_map<std::string, std::vector<pending_process>> push_requests_;
-  std::unordered_map<std::string, std::vector<pending_process>> pop_requests_;
+  std::unordered_map<std::string, pending_nodes> datas_;
 };
 
 }  // namespace tunnel
